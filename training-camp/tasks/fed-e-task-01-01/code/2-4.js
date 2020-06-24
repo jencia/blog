1d98ae4618f238c7f595ea03d7afc9404e58e060
jencia <1073956837@qq.com>
2020-06-24 22:27:32 +0800

    fix: 2-4 没有转化为小写

--- training-camp/tasks/fed-e-task-01-01/code/2-4.js
+++ training-camp/tasks/fed-e-task-01-01/code/2-4.js
@@ -4,6 +4,6 @@ const cars = require('./2-cars')
 let _underscore = fp.replace(/\W+/g, '_')
 let setPropsByFn = fp.curry((name, fn, obj) => fp.set(name, fn(obj[name]))(obj))
 
-let sanitizeNames = fp.map(setPropsByFn('name', _underscore))
+let sanitizeNames = fp.map(setPropsByFn('name', fp.flowRight(_underscore, fp.toLower)))
 
 console.log(sanitizeNames(cars))
