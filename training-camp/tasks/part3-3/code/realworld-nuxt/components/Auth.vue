58f3c7dfc8ea66507b50df46cc13e6be6f7f1b83
yangjc <yangjc@sunsharing.com.cn>
2020-09-03 13:15:34 +0800

    feat: update realworld-nuxt

--- training-camp/tasks/part3-3/code/realworld-nuxt/components/Auth.vue
+++ training-camp/tasks/part3-3/code/realworld-nuxt/components/Auth.vue
@@ -1,42 +1,79 @@
- <template>
+<template>
     <div class="auth-page">
         <div class="container page">
             <div class="row">
 
                 <div class="col-md-6 offset-md-3 col-xs-12">
-                    <h1 class="text-xs-center">Sign up</h1>
+                    <h1 class="text-xs-center">{{ isLogin ? '登录' : '注册' }}</h1>
                     <p class="text-xs-center">
-                    <a href="">Have an account?</a>
+                        <nuxt-link to="register" v-if="isLogin">还没有账号？</nuxt-link>
+                        <nuxt-link to="/login" v-else>已经有账号了？</nuxt-link>
                     </p>
 
                     <ul class="error-messages">
-                    <li>That email is already taken</li>
+                        <li>That email is already taken</li>
                     </ul>
 
-                    <form>
-                    <fieldset class="form-group">
-                        <input class="form-control form-control-lg" type="text" placeholder="Your Name">
-                    </fieldset>
-                    <fieldset class="form-group">
-                        <input class="form-control form-control-lg" type="text" placeholder="Email">
-                    </fieldset>
-                    <fieldset class="form-group">
-                        <input class="form-control form-control-lg" type="password" placeholder="Password">
-                    </fieldset>
-                    <button class="btn btn-lg btn-primary pull-xs-right">
-                        Sign up
-                    </button>
+                    <form @submit.prevent="isLogin ? login() : register()">
+                        <fieldset class="form-group" v-if="!isLogin">
+                            <input
+                                class="form-control form-control-lg"
+                                type="text"
+                                placeholder="请输入你的名称"
+                                v-model="username"
+                            >
+                        </fieldset>
+                        <fieldset class="form-group">
+                            <input
+                                class="form-control form-control-lg" type="text"
+                                placeholder="请输入邮件地址"
+                                v-model="email"
+                            >
+                        </fieldset>
+                        <fieldset class="form-group">
+                            <input
+                                class="form-control form-control-lg"
+                                type="password"
+                                placeholder="请输入密码"
+                                v-model="password"
+                            >
+                        </fieldset>
+                        <button class="btn btn-lg btn-primary pull-xs-right">
+                            {{ isLogin ? '登录' : '注册' }}
+                        </button>
                     </form>
                 </div>
 
             </div>
         </div>
     </div>
- </template>
+</template>
  
- <script>
- export default {
-     name: 'Auth'
- }
- </script>
+<script>
+import { register } from '@/api/user';
+
+export default {
+    name: 'Auth',
+    data () {
+        return {
+            username: '',
+            email: '',
+            password: ''
+        };
+    },
+    computed: {
+        isLogin () {
+            return this.$route.name === 'login'
+        }
+    },
+    methods: {
+        login () {},
+        async register () {
+            const res = await register({ user: this.$data })
+
+            console.log(res)
+        }
+    }
+}
+</script>
  
--- training-camp/tasks/part3-3/code/realworld-nuxt/layouts/default.vue
+++ training-camp/tasks/part3-3/code/realworld-nuxt/layouts/default.vue
@@ -2,25 +2,21 @@
     <div>
         <nav class="navbar navbar-light">
             <div class="container">
-                <a class="navbar-brand" href="index.html">conduit</a>
+                <nuxt-link class="navbar-brand" to="/">conduit</nuxt-link>
                 <ul class="nav navbar-nav pull-xs-right">
-                <li class="nav-item">
-                    <!-- Add "active" class when you're on that page" -->
-                    <a class="nav-link active" href="">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="">
-                    <i class="ion-compose"></i>&nbsp;New Post
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="">
-                    <i class="ion-gear-a"></i>&nbsp;Settings
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="">Sign up</a>
-                </li>
+                    <template class="nav-item" v-for="item in menus">
+                        <li :key="item.path" class="nav-item">
+                            <!-- Add "active" class when you're on that page" -->
+                            <nuxt-link
+                                class="nav-link"
+                                :class="$route.path === item.path ? 'active' : ''"
+                                :to="item.path"
+                            >
+                                <i :class="item.icon" v-if="item.icon"></i>
+                                {{ item.name }}
+                            </nuxt-link>
+                        </li>
+                    </template>
                 </ul>
             </div>
         </nav>
@@ -29,7 +25,7 @@
             <div class="container">
                 <a href="/" class="logo-font">conduit</a>
                 <span class="attribution">
-                An interactive learning project from <a href="https://thinkster.io">Thinkster</a>. Code &amp; design licensed under MIT.
+                    An interactive learning project from <a href="https://thinkster.io">Thinkster</a>. Code &amp; design licensed under MIT.
                 </span>
             </div>
         </footer>
@@ -38,6 +34,34 @@
 
 <script>
 export default {
-    name: 'DefaultLayout'
+    name: 'DefaultLayout',
+    data () {
+        return {
+            menus: [
+                {
+                    name: '首页',
+                    path: '/'
+                },
+                {
+                    name: '写文章',
+                    path: '/editor',
+                    icon: 'ion-compose'
+                },
+                {
+                    name: '设置',
+                    path: '/settings',
+                    icon: 'ion-gear-a'
+                },
+                {
+                    name: '登录',
+                    path: '/login'
+                },
+                {
+                    name: '注册',
+                    path: '/register'
+                }
+            ]
+        }
+    }
 }
 </script>
